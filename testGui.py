b2ba28fa4c0e8e3eb772102752142d91e83aa1e1
Joshua Athayde <joshua.athayde@gmail.com>
2020-08-23 20:15:43 -0700

    Made things move using Canvas!

--- testGui.py
+++ testGui.py
@@ -2,19 +2,19 @@ import tkinter as tk
 import time
 import threading
 
-class Block:
-    def __init__(self, queue, master):
-        self.queue = queue
-        block = tk.Frame(master, relief = tk.RAISED, borderwidth=2)
-        label = tk.Label(master = block, text = "1")
+class Board:
+    def __init__(self, master):
+        self.canvas = tk.Canvas(master)
+        self.block = self.canvas.create_rectangle(5,5,25,25,fill = "black")
+        self.canvas.pack()
+        self.moveRight()
+
+    def moveRight(self):
+        self.canvas.move(self.block, 5, 0)
+        self.canvas.after(100, self.moveRight)
+
+
 
 window = tk.Tk()
-frame = tk.Frame(window, relief = tk.RAISED, borderwidth=2)
-label = tk.Label(master = frame, text = "hello world")
-frame2 = tk.Frame(window, relief = tk.RAISED, borderwidth=2)
-label2 = tk.Label(master = frame2, text = "hello world")
-frame.pack()
-frame2.pack()
-label.pack()
-label2.pack()
+board = Board(window)
 window.mainloop()
