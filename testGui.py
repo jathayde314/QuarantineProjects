b25b54c44de9a713421f42bced976568c392b5fd
Joshua Athayde <joshua.athayde@gmail.com>
2020-08-24 19:08:15 -0700

    Moving block in grid partially implemented.

--- testGui.py
+++ testGui.py
@@ -2,36 +2,51 @@ import tkinter as tk
 import time
 import threading
 
-class Board:
+class GraphicsClass:
     def __init__(self, master):
         self.canvas = tk.Canvas(master)
-        self.block = self.canvas.create_rectangle(5,5,25,25,fill = "black")
+        self.block1 = self.canvas.create_rectangle(5,5,25,25,fill = "black")
         self.canvas.pack()
-        #self.moveRight()
 
-    def moveRight(self):
+    def moveBlockRight(self, block):
         self.canvas.move(self.block, 1, 0)
         self.canvas.after(100)
 
-    def moveDown(self):
+    def moveBlockDown(self, block):
         self.canvas.move(self.block, 0, 1)
         self.canvas.after(100)
 
-    def moveLeft(self):
+    def moveBlockLeft(self, block):
         self.canvas.move(self.block, -1, 0)
         self.canvas.after(100)
 
-    def moveUp(self):
+    def moveBlockUp(self, block):
         self.canvas.move(self.block, 0, -1)
 
-
+    def paintBoard(self, board):
+        self.canvas.delete(self.block1)
+        for row in range(0,4):
+            for col in range(0,4):
+                if board[row][col] != 0:
+                    self.block = self.canvas.create_rectangle(5+row*20,5+col*20,25+row*20,25+col*20,fill = "black")
 
 window = tk.Tk()
-board = Board(window)
+animatedGrid = GraphicsClass(window)
 
-window.bind("<KeyRelease-Left>", lambda e: board.moveLeft())
-window.bind("<KeyRelease-Right>", lambda e: board.moveRight())
-window.bind("<KeyRelease-Up>", lambda e: board.moveUp())
-window.bind("<KeyRelease-Down>", lambda e: board.moveDown())
+board = [[1,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+def rightMove(board):
+    for row in range(0,3):
+        for col in range(0,4):
+            if board[row][col] != 0:
+                board[row+1][col] = board[row][col]
+                board[row][col] = 0
+    animatedGrid.paintBoard(board)
+
+
+window.bind("<KeyRelease-Left>", lambda e: animatedGrid.moveBlockLeft())
+window.bind("<KeyRelease-Right>", lambda e: rightMove(board))
+window.bind("<KeyRelease-Up>", lambda e: animatedGrid.moveBlockUp())
+window.bind("<KeyRelease-Down>", lambda e: animatedGrid.moveBlockDown())
 
 window.mainloop()
