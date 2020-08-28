030a1a1ef4b46cb47a59709b4fb216272956ff9a
Joshua Athayde <joshua.athayde@gmail.com>
2020-08-28 14:46:05 -0700

    Able to use multiprocessing to control game.

--- minimax.py
+++ minimax.py
@@ -1,5 +1,22 @@
-from testGui import rightMove, downMove, leftMove, upMove, setKeyboardBindings, runGame
+from testGui import runGame
 import multiprocessing
+import tkinter
+import time
 
 q = multiprocessing.Queue()
-runGame(q)
+
+def test(q):
+    for i in range(0,5):
+        q.put("right")
+
+print(multiprocessing.get_start_method())
+if __name__ == "__main__":
+    #Necessary to run on macs
+    multiprocessing.set_start_method('spawn', force=True)
+    q = multiprocessing.Queue()
+    p1 = multiprocessing.Process(target=runGame, args=(q,))
+    p2 = multiprocessing.Process(target=test, args=(q,))
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
